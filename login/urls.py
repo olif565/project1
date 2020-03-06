1faac7dfadfb90ff8c67eed8632f586b37238757
irvanwahyu <irvanyale14@gmail.com>
2020-03-06 08:52:20 +0700

    create register layout

--- login/urls.py
+++ login/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path, include
+from login import views
+
+app_name = 'login'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('login/', include('login.urls', namespace='login'))
+    path('signup/', views.signup, name='signup'),
 ]
